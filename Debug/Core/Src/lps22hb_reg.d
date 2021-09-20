10ac59b4e5bf5a9e6d9596126a5d68925a3abe12
gusmero <andregusmara@outlook.it>
2021-09-20 11:44:36 +0200

    project's code

---
+++ Debug/Core/Src/lps22hb_reg.d
@@ -0,0 +1,4 @@
+Core/Src/lps22hb_reg.o: ../Core/Src/lps22hb_reg.c \
+ ../Core/Inc/lps22hb_reg.h
+
+../Core/Inc/lps22hb_reg.h:
---
+++ Debug/Core/Src/lsm303agr_reg.d
@@ -0,0 +1,4 @@
+Core/Src/lsm303agr_reg.o: ../Core/Src/lsm303agr_reg.c \
+ ../Core/Inc/lsm303agr_reg.h
+
+../Core/Inc/lsm303agr_reg.h:
